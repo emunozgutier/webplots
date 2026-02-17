a01413706da876fa4913dee2642dba85d3268b81
emunozgutier <emunozgutier@gmail.com>
2026-02-16 21:56:02 -0800

    Create DataStructure.ts

---
+++ src/DataStructure.ts
@@ -0,0 +1,11 @@
+
+export interface AxisMenuData {
+    xAxis: string;
+    yAxis: string;
+}
+
+export interface PlotArea {
+    enableLogAxis: boolean;
+    plotTitle: string;
+    axisMenuData: AxisMenuData;
+}
